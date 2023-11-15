65d6560e5bb14a0c441dd96d6b86053aecf41fc0
mo59578 <moritz.platen@studmail.w-hs.de>
2023-11-15 18:14:50 +0100

    Add MyDraggableScrollableWidget for BottomSheets Add Test BottomSheet for Lukas

---
+++ lib/components/bottomSheetItems/bottomSheetTest/MyBottomSheetTestItem.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class MyBottomSheetTestItem {
+
+  static List<Widget> generateBottomSheet() {
+
+    return <Widget> [
+
+      const SizedBox(
+        height: 40,
+      ),
+
+      const Center(
+        child: Text("Hallo"),
+      ),
+
+      Padding(
+        padding: const EdgeInsets.only(left: 60, right: 60),
+        child: ElevatedButton(
+            onPressed: () {
+              ///TODO: Entsprechend auslagern
+            },
+            child: const Text("Click Mich")
+        ),
+      ),
+
+    ];
+  }
+}
