c2fa1b3a7de4b8fa13bd644064ee01885518150a
moritzuni <137947261+moritzuni@users.noreply.github.com>
2023-11-23 12:23:01 +0100

    Add loginWithBiometrics

--- lib/functions/services/biometricsLogin/MyBiometricsLogin.dart
+++ lib/functions/services/biometricsLogin/MyBiometricsLogin.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:shopping_app/exceptions/MyCustomException.dart';
 import 'package:shopping_app/functions/services/biometricsAuth/MyBiometricsAuthService.dart';
 import 'package:shopping_app/functions/services/storage/MySecureStorageService.dart';
@@ -43,5 +44,35 @@ class MyBiometricsLogin {
         email: "",
         password: ""
     );
+  }
+
+  /// This function logged in the user with biometrics automatically
+  ///
+  /// [MyCustomException] Keys:
+  /// - email-password-null: email or password are null!
+  /// - email-password-empty: email or password are empty!
+  static void loginWithBiometrics() async {
+    MyDefaultUserStructure myUser = await getUserDataFromBiometrics();
+
+    if (myUser.email == null || myUser.password == null) {
+
+      throw MyCustomException("email or password are null!", "email-password-null");
+    }
+
+    if (myUser.email == "" || myUser.password == "") {
+
+      throw MyCustomException("email or password are empty!", "email-password-empty");
+    }
+
+    try {
+
+      await FirebaseAuth.instance.signInWithEmailAndPassword(email: myUser.email!, password: myUser.password!);
+    } catch (e) {
+
+      if (e is FirebaseException) {
+
+        throw MyCustomException(e.message == null ? "" : e.message!, e.code);
+      }
+    }
   }
 }
