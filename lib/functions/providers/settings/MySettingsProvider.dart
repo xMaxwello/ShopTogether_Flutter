39528082fd4a08c01b22020147cf2de1f5222e0a
xMaxwello <lukas.p9199@gmail.com>
2023-12-01 18:43:58 +0100

    Settings are locally saved

--- lib/functions/providers/settings/MySettingsProvider.dart
+++ lib/functions/providers/settings/MySettingsProvider.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
 
 class MySettingsProvider with ChangeNotifier {
   bool _isBiometricLock = false;
@@ -7,6 +9,19 @@ class MySettingsProvider with ChangeNotifier {
   bool _isSoundEnabled = true;
   bool _isDarkThemeEnabled = false;
 
+  MySettingsProvider() {
+    loadSettings();
+  }
+
+  Future<void> loadSettings() async {
+    final prefs = await SharedPreferences.getInstance();
+    _isBiometricLock = prefs.getBool('biometricLock') ?? false;
+    _isNotificationsEnabled = prefs.getBool('notificationsEnabled') ?? true;
+    _isVibrationEnabled = prefs.getBool('vibrationEnabled') ?? true;
+    _isSoundEnabled = prefs.getBool('soundEnabled') ?? true;
+    _isDarkThemeEnabled = prefs.getBool('darkThemeEnabled') ?? false;
+    notifyListeners();
+  }
 
   bool get isBiometricLock => _isBiometricLock;
   bool get isNotificationsEnabled => _isNotificationsEnabled;
@@ -15,31 +30,41 @@ class MySettingsProvider with ChangeNotifier {
   bool get isDarkThemeEnabled => _isDarkThemeEnabled;
 
 
-  void updateIsBiometricLock(bool value) {
+  void updateIsBiometricLock(bool value) async {
     _isBiometricLock = value;
     notifyListeners();
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool('biometricLock', value);
   }
 
-  void updateIsNotificationsEnabled(bool value) {
+  void updateIsNotificationsEnabled(bool value) async {
     _isNotificationsEnabled = value;
     notifyListeners();
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool('notificationsEnabled', value);
   }
 
-  void updateIsVibrationEnabled(bool value) {
+  void updateIsVibrationEnabled(bool value) async {
     _isVibrationEnabled = value;
     notifyListeners();
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool('vibrationEnabled', value);
   }
 
-  void updateIsSoundEnabled(bool value) {
+  void updateIsSoundEnabled(bool value) async {
     _isSoundEnabled = value;
     notifyListeners();
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool('soundEnabled', value);
   }
 
   ThemeMode get currentThemeMode => _isDarkThemeEnabled ? ThemeMode.dark : ThemeMode.light;
 
-  void updateIsDarkThemeEnabled(bool value) {
+  void updateIsDarkThemeEnabled(bool value) async {
     _isDarkThemeEnabled = value;
     notifyListeners();
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool('darkThemeEnabled', value);
   }
 
 }
