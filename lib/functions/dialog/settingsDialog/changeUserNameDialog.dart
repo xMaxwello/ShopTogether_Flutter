5e46c67d885c80e05f130590edb3aced77bc50e9
xMaxwello <lukas.p9199@gmail.com>
2024-01-17 21:43:30 +0100

    Current Name of user is displayed in changeUserNameDialog.dart

--- lib/functions/dialog/settingsDialog/changeUserNameDialog.dart
+++ lib/functions/dialog/settingsDialog/changeUserNameDialog.dart
@@ -1,10 +1,16 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:shopping_app/functions/dialog/MyDialog.dart';
 import 'package:shopping_app/functions/services/settings/MyAccountSettingsService.dart';
+import 'package:shopping_app/functions/services/firestore/MyFirestoreService.dart';
+import 'package:shopping_app/objects/users/MyUsers.dart';
 
-void changeUserNameDialog(BuildContext context) {
-  final TextEditingController newPrenameController = TextEditingController();
-  final TextEditingController newSurnameController = TextEditingController();
+void changeUserNameDialog(BuildContext context) async {
+  final String userUuid = FirebaseAuth.instance.currentUser!.uid;
+  final MyUser user = await MyFirestoreService.userService.getUserAsObject(userUuid);
+
+  final TextEditingController newPrenameController = TextEditingController(text: user.prename);
+  final TextEditingController newSurnameController = TextEditingController(text: user.surname);
   final TextEditingController passwordController = TextEditingController();
   final MyAccountSettingsService service = MyAccountSettingsService();
 
