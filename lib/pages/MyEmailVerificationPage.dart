babe36b75f853dd39a1a68b2a45e4d93af56f139
Moritz Platen <72431674+MoritzPlaten@users.noreply.github.com>
2024-01-26 21:33:59 +0100

    Update

--- lib/pages/MyEmailVerificationPage.dart
+++ lib/pages/MyEmailVerificationPage.dart
@@ -17,26 +17,30 @@ class MyEmailVerificationAuthPage extends StatefulWidget {
 
 class _MyEmailVerificationAuthPageState extends State<MyEmailVerificationAuthPage> {
 
-  int counter = 0;
+  late Timer _timer;
 
-  Stream<bool> checkUserVerified() async* {
-    bool verified = false;
-    while (!verified) {
-      User? user = FirebaseAuth.instance.currentUser;
-      if(user != null) await user.reload();
-      if (user == null) {
-        yield false;
-      } else {
-        await user.reload();
-        bool verified = user.emailVerified;
-        if (verified && counter == 0) {
-          Navigator.push(context, MaterialPageRoute(builder: (context) => const MyHomePage()));
-          //MySnackBarService.showMySnackBar(context, "Ihre E-Mail wurde verifiziert!", isError: false);
-          counter = 1;
-        }
-        yield verified;
+  @override
+  void initState() {
+    super.initState();
+
+    _timer = Timer.periodic(const Duration(milliseconds: 500), (timer)  {
+
+      FirebaseAuth.instance.currentUser?.reload().then((value) => null);
+      final user = FirebaseAuth.instance.currentUser;
+      if (user?.emailVerified ?? false) {
+        timer.cancel();
+
+        //MySnackBarService.showMySnackBar(context, "Ihre E-Mail wurde verifiziert!", isError: false);
+        Navigator.push(context, MaterialPageRoute(builder: (context) => const MyHomePage()));
       }
-    }
+    });
+
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _timer.cancel();
   }
 
   @override
@@ -51,110 +55,90 @@ class _MyEmailVerificationAuthPageState extends State<MyEmailVerificationAuthPag
                 : 'assets/background.png';
           }
 
-          return StreamBuilder<bool>(
-              stream: checkUserVerified(),
-              builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
+          return Scaffold(
+            body: Stack(
+              children: [
 
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return const Center(
-                    child: CircularProgressIndicator(),
-                  );
-                }
+                Container(
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage(getBackgroundImage()),
+                      fit: BoxFit.cover,),),
+                ),
 
-                if (!snapshot.hasData) {
-                  return const Center(
-                    child: CircularProgressIndicator(),
-                  );
-                }
-
-                //requires-recent-login
-
-                return Scaffold(
-                  body: Stack(
-                    children: [
-
-                      Container(
-                        decoration: BoxDecoration(
-                          image: DecorationImage(
-                            image: AssetImage(getBackgroundImage()),
-                            fit: BoxFit.cover,),),
+                Center(
+                  child: Card(
+                    color: Theme.of(context).cardTheme.color,
+                    elevation: 20,
+                    clipBehavior: Clip.antiAlias,
+                    shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(20.0),
+                          bottomRight: Radius.circular(20.0),
+                          topLeft: Radius.circular(20.0),
+                          bottomLeft: Radius.circular(20.0)
                       ),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(25),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
 
-                      Center(
-                        child: Card(
-                          color: Theme.of(context).cardTheme.color,
-                          elevation: 20,
-                          clipBehavior: Clip.antiAlias,
-                          shape: const RoundedRectangleBorder(
-                            borderRadius: BorderRadius.only(
-                                topRight: Radius.circular(20.0),
-                                bottomRight: Radius.circular(20.0),
-                                topLeft: Radius.circular(20.0),
-                                bottomLeft: Radius.circular(20.0)
+                          Text(
+                            "E-Mail Adresse verifizieren",
+                            style: Theme.of(context).textTheme.titleMedium,
+                          ),
+
+                          const SizedBox(height: 30,),
+                          Text(
+                            "Bevor Sie die App benutzen können, müssen\n Sie Ihre E-Mail Adresse verifizieren!",
+                            style: Theme.of(context).textTheme.bodyMedium,
+                            softWrap: true,
+                            textAlign: TextAlign.center,
+                          ),
+
+                          const SizedBox(height: 20,),
+
+                          ElevatedButton(
+                            onPressed: () async {
+
+                              User? user = FirebaseAuth.instance.currentUser;
+
+                              if (user != null) {
+
+                                await user.sendEmailVerification();
+                                MySnackBarService.showMySnackBar(context, 'Die Verifizierungs-E-Mail wurde versendet!', isError: false,);
+                              } else {
+                                MySnackBarService.showMySnackBar(context, 'Sie sind nicht angemeldet!');
+                              }
+                            },
+                            child: Text(
+                                "E-Mail erneut senden",
+                                style: Theme.of(context).textTheme.displaySmall
                             ),
                           ),
-                          child: Padding(
-                            padding: const EdgeInsets.all(25),
-                            child: Column(
-                              mainAxisSize: MainAxisSize.min,
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
 
-                                Text(
-                                  "E-Mail Adresse verifizieren",
-                                  style: Theme.of(context).textTheme.titleMedium,
-                                ),
-
-                                const SizedBox(height: 30,),
-                                Text(
-                                  "Bevor Sie die App benutzen können, müssen\n Sie Ihre E-Mail Adresse verifizieren!",
-                                  style: Theme.of(context).textTheme.bodyMedium,
-                                  softWrap: true,
-                                  textAlign: TextAlign.center,
-                                ),
-
-                                const SizedBox(height: 20,),
-
-                                ElevatedButton(
-                                  onPressed: () async {
-
-                                    User? user = FirebaseAuth.instance.currentUser;
-
-                                    if (user != null) {
-
-                                      await user.sendEmailVerification();
-                                      MySnackBarService.showMySnackBar(context, 'Die Verifizierungs-E-Mail wurde versendet!', isError: false,);
-                                    } else {
-                                      MySnackBarService.showMySnackBar(context, 'Sie sind nicht angemeldet!');
-                                    }
-                                  },
-                                  child: Text(
-                                      "E-Mail erneut senden",
-                                      style: Theme.of(context).textTheme.displaySmall
-                                  ),
-                                ),
-
-                                ElevatedButton(
-                                  onPressed: () async {
-                                    await FirebaseAuth.instance.signOut();
-                                  },
-                                  child: Text(
-                                      "Abbruch",
-                                      style: Theme.of(context).textTheme.displaySmall
-                                  ),
-                                ),
-
-                              ],
+                          ElevatedButton(
+                            onPressed: () async {
+                              await FirebaseAuth.instance.signOut();
+                            },
+                            child: Text(
+                                "Abbruch",
+                                style: Theme.of(context).textTheme.displaySmall
                             ),
                           ),
-                        ),
-                      )
 
-                    ],
+                        ],
+                      ),
+                    ),
                   ),
-                );
-              }
+                )
+
+              ],
+            ),
           );
         }
     );
